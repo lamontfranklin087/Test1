2f716b1307615a8d5aa9610f772b5a0afff1fbc8
Lamont Franklin <lamont87@uw.edu>
2016-05-29 18:38:35 -0700

    Initial commit

---
+++ app/src/main/java/com/example/lamont/test1/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.lamont.test1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button myButton = (Button) findViewById(R.id.my_button);
+    }
+}
